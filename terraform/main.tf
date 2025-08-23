1439d041f66887753e7864cd21ede2a1440458e8
Akwa B. NJIE <akwaking@gmail.com>
2025-08-23 16:19:07 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,35 @@
-resource "aws_s3_bucket" "site" {
-  bucket = var.bucket_name
-  # Remove acl = "public-read"
+provider "aws" {
+  region = "us-east-2" # 👈 adjust to the region you want
 }
 
+# S3 Bucket
+resource "aws_s3_bucket" "site" {
+  bucket = var.bucket_name
+}
+
+# Disable Block Public Access
+resource "aws_s3_bucket_public_access_block" "site" {
+  bucket                  = aws_s3_bucket.site.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Public Read Policy
 resource "aws_s3_bucket_policy" "site_policy" {
   bucket = aws_s3_bucket.site.id
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Sid       = "PublicReadGetObject"
-      Effect    = "Allow"
-      Principal = "*"
-      Action    = "s3:GetObject"
-      Resource  = "${aws_s3_bucket.site.arn}/*"
-    }]
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.site.arn}/*"
+      }
+    ]
   })
 }
-
